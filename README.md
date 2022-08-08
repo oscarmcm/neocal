57703adff3dfc066f32e9bbfb85b46ae0d6e1987
Oscar Cortez <om.cortez.2010@gmail.com>
2022-08-08 16:47:44 -0600

    Improve agenda view implementation

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# neocal
- Google Calendar Command Line Interface
+# NeoCal - Google Calendar Command Line Interface
+
+**NeoCal** is a Rust application that allows you to access your Google Calendar(s) from a command line.
+
+## Installation
+
+### SetUp
+
+## HowTo
+
+## ToDo
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,34 @@
-use configparser::ini::Ini;
-use std::error::Error;
 use std::env;
+use std::error::Error;
+use std::path::Path;
+
+use chrono::DateTime;
+use chrono::prelude::*;
+use configparser::ini::Ini;
 use clap::Parser;
-use serde::{Deserialize, Serialize};
+use home;
 use reqwest;
+use serde::{Deserialize, Serialize};
 use term_table::TableStyle;
 use term_table::Table;
 use term_table::row::Row;
 use term_table::table_cell::{TableCell, Alignment};
+use term_size;
+
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Event {
     summary: String,
     description: String,
-    start: String,
-    end: String,
+    start_date: String,
+    start_date_time: String,
+    end_date: String,
+    end_date_time: String,
     call: String,
 }
 
 #[derive(Parser, Debug)]
-#[clap(author="Oscar Cortez", version, about="Google Calendar CLI", long_about = None)]
+#[clap(author="Oscar Cortez <om.cortez.2010@gmail.com>", version, about="Google Calendar CLI", long_about = None)]
 struct Args {
    /// Type of the calendar view to use
    view: Option<String>,
@@ -28,8 +37,6 @@ struct Args {
    #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_calendar_name)]
    calendar: Option<String>,
 }
-
-
 
 fn validate_calendar_name(name: &str) -> Result<(), String> {
     if name.trim().len() != name.len() {
@@ -43,20 +50,51 @@ fn validate_calendar_name(name: &str) -> Result<(), String> {
 
 fn agenda_view(events: Vec<Event>) {
     let mut table = Table::new();
-    table.style = TableStyle::extended();
-    table.max_column_width = 40;
+    let mut event_date = "";
+
+    table.style = TableStyle::blank();
+    table.max_column_width =
+        if let Some((w, _h)) = term_size::dimensions() {
+            w - 60
+        } else {
+            80
+        };
+
     for event in events.iter() {
-        table.add_row(Row::new(vec![
-            TableCell::new(&event.summary),
-            TableCell::new(&event.description)
-        ]));
+        let event_time =
+            if &event.start_date_time != "" && &event.end_date_time != "" {
+                format!(
+                    "{} - {}",
+                    DateTime::parse_from_rfc3339(&event.start_date_time).unwrap().format("%H:%M").to_string(),
+                    DateTime::parse_from_rfc3339(&event.end_date_time).unwrap().format("%H:%M").to_string(),
+                )
+            } else {
+                "All Day".to_string()
+            };
+
+        let mut table_row = [
+            TableCell::new(""),
+            TableCell::new_with_alignment(event_time, 1, Alignment::Right),
+            TableCell::new_with_alignment(&event.summary, 1, Alignment::Left),
+        ];
+        if event_date != &event.start_date {
+            table.add_row(Row::new(vec![
+                TableCell::new(""),
+                TableCell::new(""),
+                TableCell::new(""),
+            ]));
+            table_row[0] = TableCell::new(&event.start_date);
+        };
+        table.add_row(Row::new(table_row));
+        event_date = &event.start_date;
     };
+
     println!("{}", table.render());
 }
 
-fn calendar_view(events: Vec<Event>) {
-    println!("Calendar View");
-}
+//fn calendar_view(events: Vec<Event>) {
+//    println!("Calendar View");
+//}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -66,7 +104,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let config_path = match env::var("NEOCAL_CONFIG_PATH") {
         Ok(val) => val,
-        Err(_e) => ".config/neocal/config.ini".to_string(),
+        Err(_e) => {
+            match home::home_dir() {
+                Some(path) => Path::join(&path, Path::new("./.config/neocal/config.ini")).to_str().unwrap().to_string(),
+                None => panic!("Impossible to get your home dir!"),
+            }
+        },
     };
     config.load(config_path)?;
 
@@ -92,7 +135,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 Ok(events) => {
                     match view_to_use.as_str() {
                         "agenda" => agenda_view(events),
-                        "calendar" => calendar_view(events),
+                        "calendar" => println!("Not implemented at this moment."),
                         _ => println!("Unrecognized view type"),
                     };
                 },
@@ -106,7 +149,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             println!("Looks like the Calendar URL is not public.");
         },
         _ => {
-            panic!("Uh oh! Something unexpected happened.");
+            println!("Uh oh! Something unexpected happened.");
         },
     };
 
