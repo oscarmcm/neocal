418a793daacf9c38f3686fe66154361f8640b1d4
Oscar Cortez <om.cortez.2010@gmail.com>
2022-08-10 21:42:35 -0600

    Initial work on rebuild commands configuration

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::env;
 use std::error::Error;
 use std::path::Path;
 
-use chrono::DateTime;
-use clap::Parser;
+use chrono::{DateTime, Datelike, NaiveDate, Weekday};
+use clap::{Parser, Subcommand};
 use configparser::ini::Ini;
 use home;
 use reqwest;
@@ -27,23 +27,87 @@ struct Event {
     call: String,
 }
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[clap(author="Oscar Cortez <om.cortez.2010@gmail.com>", version, about="Google Calendar CLI", long_about = None)]
-struct Args {
-    /// Type of the calendar view to use
-    view: Option<String>,
-
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+    #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_option_value)]
     /// Name of the calendar to use
-    #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_option_value)]
-    calendar: Option<String>,
+    r#for: Option<String>,
 
-    /// Word to search in the calendar
     #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_option_value)]
+    /// Word to search in the calendar
     search: Option<String>,
 
-    /// Name of the Time Zone to return the events
     #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_option_value)]
+    /// Name of the Time Zone to return the events
     timezone: Option<String>,
+
+    #[clap(long)]
+    /// Get calendar entries for the current week
+    week: bool,
+
+    #[clap(long)]
+    /// Get calendar entries for today
+    today: bool,
+
+    #[clap(long)]
+    /// Get calendar entries for tomorrow
+    tomorrow: bool,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Shows user calendar in Agenda-like view mode
+    Agenda {
+        #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_option_value)]
+        /// Name of the calendar to use
+        r#for: Option<String>,
+
+        #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_option_value)]
+        /// Word to search in the calendar
+        search: Option<String>,
+
+        #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_option_value)]
+        /// Name of the Time Zone to return the events
+        timezone: Option<String>,
+
+        #[clap(long)]
+        /// Get calendar entries for the current week
+        week: bool,
+
+        #[clap(long)]
+        /// Get calendar entries for today
+        today: bool,
+
+        #[clap(long)]
+        /// Get calendar entries for tomorrow
+        tomorrow: bool,
+    },
+    /// Show user calendar in Calendar-like view mode
+    Calendar {
+        #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_option_value)]
+        /// Name of the calendar to use
+        r#for: Option<String>,
+
+        #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_option_value)]
+        /// Word to search in the calendar
+        search: Option<String>,
+
+        #[clap(short, long, value_parser, forbid_empty_values = true, validator = validate_option_value)]
+        /// Name of the Time Zone to return the events
+        timezone: Option<String>,
+    },
+}
+
+fn current_week_bounds() -> (NaiveDate, NaiveDate) {
+    let current_year = chrono::offset::Local::now().year();
+    let current_week = chrono::offset::Local::now().iso_week().week();
+    let mon = NaiveDate::from_isoywd(current_year, current_week, Weekday::Mon);
+    let sun = NaiveDate::from_isoywd(current_year, current_week, Weekday::Sun);
+    (mon, sun)
 }
 
 fn validate_option_value(name: &str) -> Result<(), String> {
@@ -56,9 +120,11 @@ fn validate_option_value(name: &str) -> Result<(), String> {
     }
 }
 
-fn agenda_view(events: Vec<Event>) {
+fn request_calendar_endpoint() {}
+
+fn render_agenda_view(events: Vec<Event>) -> std::io::Result<()> {
     if events.iter().len() == 0 {
-        println!("No Events were found.");
+        eprintln!("No Events were found.");
     };
 
     let mut table = Table::new();
@@ -91,7 +157,11 @@ fn agenda_view(events: Vec<Event>) {
         let mut table_row = [
             TableCell::new(""),
             TableCell::new_with_alignment(event_time, 1, Alignment::Right),
-            TableCell::new_with_alignment(&event.summary, 1, Alignment::Left),
+            TableCell::new_with_alignment(
+                format!("{}\n{}", &event.summary, &event.call),
+                1,
+                Alignment::Left,
+            ),
         ];
         if event_date != &event.start_date {
             table.add_row(Row::new(vec![
@@ -106,50 +176,31 @@ fn agenda_view(events: Vec<Event>) {
     }
 
     println!("{}", table.render());
+    return Ok(());
 }
 
-//fn calendar_view(events: Vec<Event>) {
-//    println!("Calendar View");
-//}
+fn render_calendar_view(events: Vec<Event>) -> std::io::Result<()> {
+    eprintln!("This command is not yet implemented.");
+    return Ok(());
+}
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
+async fn get_events(
+    view_to_render: &str,
+    url: &str,
+    search: &str,
+    timezone: &str,
+    week: &bool,
+    today: &bool,
+    tomorrow: &bool,
+) -> Result<(), Box<dyn Error>> {
+    let mut endpoint = Url::parse(url)?;
     let client = reqwest::Client::new();
-    let mut config = Ini::new();
 
-    let config_path = match env::var("NEOCAL_CONFIG_PATH") {
-        Ok(val) => val,
-        Err(_e) => match home::home_dir() {
-            Some(path) => Path::join(&path, Path::new("./.config/neocal/config.ini"))
-                .to_str()
-                .unwrap()
-                .to_string(),
-            None => panic!("Impossible to get your home dir!"),
-        },
+    if search.len() != 0 {
+        endpoint.query_pairs_mut().append_pair("q", search);
     };
-    config.load(config_path)?;
-
-    let calendar_to_use = args
-        .calendar
-        .unwrap_or(config.get("neocal", "default").unwrap());
-    let view_to_use = args.view.unwrap_or(config.get("neocal", "mode").unwrap());
-    let timezone = args.timezone.unwrap_or(
-        config
-            .get(&calendar_to_use, "timezone")
-            .unwrap_or(config.get("neocal", "timezone").unwrap_or("".to_string())),
-    );
-    let mut endpoint = Url::parse(&config.get(&calendar_to_use, "endpoint").unwrap())?;
-
-    if args.search.is_none() == false {
-        endpoint
-            .query_pairs_mut()
-            .append_pair("q", &args.search.as_ref().unwrap().to_string());
-    };
-    if timezone.to_string().len() > 0 {
-        endpoint
-            .query_pairs_mut()
-            .append_pair("timeZone", &timezone.to_string());
+    if timezone.len() != 0 {
+        endpoint.query_pairs_mut().append_pair("timeZone", timezone);
     };
 
     let request = client
@@ -161,26 +212,178 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .unwrap();
 
     match request.status() {
-        reqwest::StatusCode::OK => {
-            match request.json::<Vec<Event>>().await {
-                Ok(events) => {
-                    match view_to_use.as_str() {
-                        "agenda" => agenda_view(events),
-                        "calendar" => println!("Not implemented at this moment."),
-                        _ => println!("Unrecognized view type"),
-                    };
-                }
-                Err(err) => println!("The response didn't match the shape we expected. {:?}", err),
+        reqwest::StatusCode::OK => match request.json::<Vec<Event>>().await {
+            Ok(events) => match view_to_render {
+                "agenda" => render_agenda_view(events),
+                "calendar" => render_calendar_view(events),
+                _ => Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Unrecognized view type",
+                )),
+            },
+            Err(err) => Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("The response didn't match the shape we expected. {}", err),
+            )),
+        },
+        reqwest::StatusCode::NOT_FOUND => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Looks like the Calendar URL does not exists.",
+        )),
+        reqwest::StatusCode::UNAUTHORIZED => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Looks like the Calendar URL is not public.",
+        )),
+        _ => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Uh oh! Something unexpected happened.",
+        )),
+    };
+    return Ok(());
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut config = Ini::new();
+    let config_path = match env::var("NEOCAL_CONFIG_PATH") {
+        Ok(val) => val,
+        Err(_e) => match home::home_dir() {
+            Some(path) => Path::join(&path, Path::new("./.config/neocal/config.ini"))
+                .to_str()
+                .unwrap()
+                .to_string(),
+            None => "".to_string(),
+        },
+    };
+    if config_path.len() == 0 {
+        eprintln!("Impossible to get your home dir!");
+        panic!();
+    };
+    config.load(config_path)?;
+
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Agenda {
+            r#for,
+            search,
+            timezone,
+            week,
+            today,
+            tomorrow,
+        }) => {
+            let calendar_to_use = if r#for.is_none() {
+                config.get("neocal", "default").unwrap()
+            } else {
+                r#for.as_ref().unwrap().to_string()
             };
+            let url = &config.get(&calendar_to_use, "endpoint").unwrap();
+
+            let mut selected_timezone = timezone.as_ref().unwrap_or(&"".to_string()).to_string();
+            if selected_timezone.len() == 0 {
+                selected_timezone = config.get(&calendar_to_use, "timezone").unwrap_or("".to_string()).to_string();
+            };
+            if selected_timezone.len() == 0 {
+                selected_timezone = config.get("neocal", "timezone").unwrap_or("".to_string()).to_string();
+            };
+            if selected_timezone.len() == 0 {
+                selected_timezone = "".to_string();
+            };
+
+            match get_events(
+                &String::from("agenda"),
+                url,
+                &search.as_ref().unwrap_or(&"".to_string()).to_string(),
+                &selected_timezone,
+                &week,
+                &today,
+                &tomorrow,
+            )
+            .await
+            {
+                Ok(_) => {}
+                Err(err) => eprintln!("Error rendering Agenda - {}", err),
+            }
         }
-        reqwest::StatusCode::NOT_FOUND => {
-            println!("Looks like the Calendar URL does not exists.");
+        Some(Commands::Calendar {
+            r#for,
+            search,
+            timezone,
+        }) => {
+            let calendar_to_use = if r#for.is_none() {
+                config.get("neocal", "default").unwrap()
+            } else {
+                r#for.as_ref().unwrap().to_string()
+            };
+            let url = &config.get(&calendar_to_use, "endpoint").unwrap();
+
+            let mut selected_timezone = timezone.as_ref().unwrap_or(&"".to_string()).to_string();
+            if selected_timezone.len() == 0 {
+                selected_timezone = config.get(&calendar_to_use, "timezone").unwrap_or("".to_string()).to_string();
+            };
+            if selected_timezone.len() == 0 {
+                selected_timezone = config.get("neocal", "timezone").unwrap_or("".to_string()).to_string();
+            };
+            if selected_timezone.len() == 0 {
+                selected_timezone = "".to_string();
+            };
+
+            match get_events(
+                &String::from("calendar"),
+                url,
+                &search.as_ref().unwrap_or(&"".to_string()).to_string(),
+                &selected_timezone,
+                &false,
+                &false,
+                &false,
+            )
+            .await
+            {
+                Ok(_) => {}
+                Err(err) => eprintln!("Error rendering Calendar - {}", err),
+            }
         }
-        reqwest::StatusCode::UNAUTHORIZED => {
-            println!("Looks like the Calendar URL is not public.");
-        }
-        _ => {
-            println!("Uh oh! Something unexpected happened.");
+        None => {
+            let view_to_use = config.get("neocal", "mode").unwrap();
+            let calendar_to_use = cli
+                .r#for
+                .unwrap_or(config.get("neocal", "default").unwrap());
+            let url = &config.get(&calendar_to_use, "endpoint").unwrap();
+            let timezone = cli.timezone.unwrap_or(
+                config
+                    .get(&calendar_to_use, "timezone")
+                    .unwrap_or(config.get("neocal", "timezone").unwrap_or("".to_string())),
+            );
+            match view_to_use.as_str() {
+                "agenda" => match get_events(
+                    &String::from("agenda"),
+                    url,
+                    &cli.search.unwrap_or("".to_string()).to_string(),
+                    &timezone,
+                    &cli.week,
+                    &cli.today,
+                    &cli.tomorrow,
+                )
+                .await
+                {
+                    Ok(_) => {}
+                    Err(err) => eprintln!("Error rendering Agenda - {}", err),
+                },
+                "calendar" => match get_events(
+                    &String::from("calendar"),
+                    url,
+                    &cli.search.unwrap_or("".to_string()).to_string(),
+                    &timezone,
+                    &false,
+                    &false,
+                    &false,
+                )
+                .await
+                {
+                    Ok(_) => {}
+                    Err(err) => eprintln!("Error rendering Calendar - {}", err),
+                },
+                _ => eprintln!("Unrecognized view type."),
+            }
         }
     };
 
